2ecf206983a55f53c463d57f97dbd25ce4b73891
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-24 19:03:43 +0500

    💚 modified: Mediator is Best

--- ChatService/MediatR/Chat/Notifications/ChatHubConnectedEvent.cs
+++ ChatService/MediatR/Chat/Notifications/ChatHubConnectedEvent.cs
@@ -1,17 +1,33 @@
-﻿using MediatR;
+﻿using ChatService.Domain.Common;
+using ChatService.Domain.Entities;
+using ChatService.Domain.Models;
+using ChatService.Hubs;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Serilog;
+using Domain;
 
 namespace ChatService.MediatR.Chat.Notifications
 {
-    public class ChatHubConnectedEvent : INotification
-    { }
+    public class ChatHubConnectedEvent : BaseEvent
+    {
+        public User user { get; set; }
+        public Room room { get; set; }
+        public readonly IHubContext<ChatHub> _context;
+        public ChatHubConnectedEvent(IHubContext<ChatHub> context)
+        {
+            _context = context;
+        }
+    }
 
     public class ChatHubConnectedEventHandler : INotificationHandler<ChatHubConnectedEvent>
     {
-        public Task Handle(ChatHubConnectedEvent Event,CancellationToken cancellationToken)
+        public async Task Handle(ChatHubConnectedEvent Event, CancellationToken cancellationToken)
         {
             Log.Information("| - - - - ChatService messaged: Connected Event - - - - |");
-            return Task.CompletedTask;
+            await Event._context.Clients.Group(Event.room.Name)
+                           .SendAsync(HubMethods.ReceiveMessage, $"Someone with ID '{Event.user.Username}' has connected");
         }
     }
 
