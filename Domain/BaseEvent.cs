1a966a34291f45aa9fa3137ae7f44fb1a3340a41
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-24 19:07:28 +0500

    🏗️ added: New Layer

---
+++ Domain/BaseEvent.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Domain
+{
+    public class BaseEvent:INotification
+    { 
+
+    }
+}
---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatService.Domain.Common
+{
+    public class BaseEntity
+    {
+        public List<BaseEvent> Events = new() { };
+
+        
+        public void AddDomainEvent(BaseEvent domainEvent)
+            => Events.Add(domainEvent);
+
+        public void RemoveDomainEvent(BaseEvent domainEvent)
+            => Events.Remove(domainEvent);
+
+        public void ClearDomainEvents()
+            => Events.Clear();
+    }
+}
