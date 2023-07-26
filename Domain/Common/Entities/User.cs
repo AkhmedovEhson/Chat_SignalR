0444a826588689d57db08bacb8ae5e5c9e9ba86c
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-26 17:06:44 +0500

    ♻️ refactor: These properties should be able to contain `NULL`

--- Domain/Common/Entities/User.cs
+++ Domain/Common/Entities/User.cs
@@ -5,10 +5,10 @@ namespace ChatService.Domain.Entities
     public class User:BaseEntity
     {
         public int Id { get; set; } 
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
 
-        public List<Room> Rooms { get; set; }
+        public List<Room>? Rooms { get; set; }
 
     }
 }
