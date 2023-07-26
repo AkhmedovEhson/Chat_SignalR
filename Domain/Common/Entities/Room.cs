150b01e3a2424b91bcb257b81ac219fcd16090ec
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-26 14:08:39 +0500

    🔨 added: Entities

---
+++ Domain/Common/Entities/Room.cs
@@ -0,0 +1,13 @@
+﻿using ChatService.Domain.Common;
+using Domain.Common;
+
+namespace ChatService.Domain.Entities
+{
+    public class Room:BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Listeners { get; set; }
+
+    }
+}
---
+++ Domain/Common/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using ChatService.Domain.Common;
+
+namespace ChatService.Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public int Id { get; set; } 
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public List<Room> Rooms { get; set; }
+
+    }
+}
