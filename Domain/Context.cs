17002522c7d27a1f8a0f3a3a188cf23ed629277a
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-26 14:09:29 +0500

    added: IHubContext and HubCallerContext must be converted to one Table

---
+++ Domain/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Context
+    {
+        public HubCallerContext HubCallerContext { get; set; }
+        public Hub IHubContext { get; set; }
+
+        public Context(HubCallerContext hubCallerContext,Hub context)
+        {
+            HubCallerContext = hubCallerContext;
+            var q = context.Context;
+            
+        }
+
+
+    }
+}
