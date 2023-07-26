c10fd7d8d727f361267ba8b7d4f6a3dfa4d71e30
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-26 14:10:41 +0500

    🔨 added: Dtos

---
+++ Domain/Dtos/UserConnection.cs
@@ -0,0 +1,10 @@
+﻿using ChatService.Domain.Entities;
+
+namespace ChatService.Domain.Models
+{
+    public class UserConnection
+    {
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+    }
+}
