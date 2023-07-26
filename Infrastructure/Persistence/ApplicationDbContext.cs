cfeb59b7c132be9c57816140e915d56b7b028e9a
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-07-26 14:07:13 +0500

    Persistence should be divided

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ChatService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Application.Common.Interfaces;
+using System.Reflection;
+
+namespace ChatService.Persistence
+{
+    public class ApplicationDbContext:DbContext,IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5434;Userid=postgres;Password=postgres;" +
+                    "Pooling=false;MinPoolSize=1;MaxPoolSize=20;Timeout=15;SslMode=Disable;Database=chat;");
+
+            }
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Infrastructure/Persistence/DbSeeder.cs
@@ -0,0 +1,35 @@
+﻿using ChatService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatService.Persistence
+{
+    public class DbSeeder
+    {
+        private readonly ModelBuilder builder;
+
+        public DbSeeder(ModelBuilder builder)
+            => this.builder = builder;
+
+        public DbSeeder Seed() => this;
+
+        public DbSeeder UserA()
+        {
+            return CreateUser(new User()
+            {
+                Id = 1,
+                Username = "UserA",
+                Password = "password",
+                Rooms = new List<Room> { new Room() { Id = 1,Name = "RoomA"} }
+            });
+
+        }
+
+
+        public DbSeeder CreateUser(User user)
+        {
+            
+            builder.Entity<User>().OwnsOne(e => e.Rooms).HasData(user);
+            return this;
+        }
+    }
+}
